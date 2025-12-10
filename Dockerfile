681a99045d602057eeb25d3f892ef99341226e51
Zlatin Hristov <zlatin.hristov@softavis.com>
2025-12-10 21:26:55 +0200

    feat: add custom configuration and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM postgres:17.7-alpine
+
+COPY postgres.conf /etc/postgresql/postgresql.conf
+
+COPY entrypoint.sh /docker-entrypoint-initdb.d/99-create-extension.sh
---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+EOSQL
